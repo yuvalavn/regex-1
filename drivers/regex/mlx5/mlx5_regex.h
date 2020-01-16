be50fa6f244a9a7b8f352b95adb53af6b50a7989
Yuval Avnery <yuvalav@mellanox.com>
2020-01-16 16:52:50 +0000

    regex/mlx5: add database commands support
    Add regex database commands:
     - Stop engine
     - Set DB umem
     - Resume
    
    Signed-off-by: Yuval Avnery <yuvalav@mellanox.com>

--- drivers/regex/mlx5/mlx5_regex.h
+++ drivers/regex/mlx5/mlx5_regex.h
@@ -5,4 +5,31 @@
 #ifndef MLX5_REGEX_H
 #define MLX5_REGEX_H
 
+#include <mlx5_devx_cmds.h>
+
+struct mlx5_database_ctx {
+	uint32_t umem_id;
+	uint64_t offset;
+};
+
+/*
+ * Sets the database address.
+ * When the database umem will be freed,
+ * the database will no longer be available to the HW.
+ */
+int mlx5_regex_database_set(struct ibv_context *ctx, int engine_id,
+			    struct mlx5_database_ctx *db_ctx);
+int mlx5_regex_database_query(struct ibv_context *ctx, int engine_id,
+			      struct mlx5_database_ctx *db_ctx);
+
+/*
+ * Stop engine after all outstanding jobs to this engine are done.
+ */
+int mlx5_regex_engine_stop(struct ibv_context *ctx, int engine_id);
+
+/*
+ * Return engine to work.
+ */
+int mlx5_regex_engine_resume(struct ibv_context *ctx, int engine_id);
+
 #endif /* MLX5_REGEX_H */
